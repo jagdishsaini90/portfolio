bd693ab5db50316a2d8e35c027ee8d7069e06ed9
Jagdish Saini <jsaini8941@gmail.com>
2021-10-06 21:39:09 +0530

    portfolio

--- src/components/Blogs/index.js
+++ src/components/Blogs/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import blog1 from "../../images/blog1.png";
 
 const index = () => {
   return (
-    <div>
-      
+    <div className="blog">
+      <h1
+        style={{
+          margin: "30px 0 30px 0",
+        }}
+      >
+        My Blogs
+      </h1>
+      <a
+        className="blog__link"
+        href="https://dev.to/jagdishsaini90/how-to-use-material-ui-media-queries-with-reactjs-4e4p"
+      >
+        <img src={blog1} alt="Blog" style={{ width: "100%", height: "100%" }} />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
--- src/components/Projects/project.card.js
+++ src/components/Projects/project.card.js
@@ -8,10 +8,10 @@ const Card = ({ doc }) => {
       <h2>{doc.name}</h2>
       <h5 style={{ margin: "10px 0 10px 0" }}>{doc.date}</h5>
       <h3 style={{ color: "grey" }}>{doc.description}</h3>
-      <a href={doc.link} style={{cursor:"pointer"}}>
+      <a href={doc.link} style={{ cursor: "pointer" }}>
         <button className="live__button">Live Project</button>
       </a>
-      <a href={doc.github} style={{cursor:"pointer"}}>
+      <a href={doc.github} style={{ cursor: "pointer" }}>
         <button className="github__button">View Source</button>
       </a>
     </div>

7d81df5d3285328181d75f8fa13d71a495c38297
Jagdish Saini <jsaini8941@gmail.com>
2021-11-17 20:52:44 +0530

    lazy React

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React,{ lazy,Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Loading from './loading/loading';
+const App = lazy(() => import("./App"))
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/loading/loading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
